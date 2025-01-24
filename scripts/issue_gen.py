5d8d7cf5efae76961fa49a354111321d470e6bfc
SnailDragon <tylernielsenn@gmail.com>
2025-01-23 22:26:39 -0700

    working on issue_gen

---
+++ scripts/issue_gen.py
@@ -0,0 +1,10 @@
+"""
+Generate issues for sensor packeting
+Execute from /scripts 
+"""
+import os 
+
+milestone = "Packet System (FSW)"
+label = "fsw"
+
+sensor_classes_path = os.path.join("..","ascendfsw","include")
