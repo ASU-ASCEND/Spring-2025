f3d9c8781bbf322df1b996e76565b2594091ca47
BryceVerberne <bryceverberne@gmail.com>
2025-03-10 18:05:29 -0700

    Implement file system (in-progress)

--- ascendfsw/src/FlashStorage.cpp
+++ ascendfsw/src/FlashStorage.cpp
@@ -3,10 +3,10 @@
 /**
  * @brief Constructs a new FlashStorage object.
  *
- * Initializes the internal write position to 0 and sets the storage name to
+ * Initializes the internal write address to 0 and sets the storage name to
  * "Flash Storage."
  */
-FlashStorage::FlashStorage() : position(0), Storage("Flash Storage") {}
+FlashStorage::FlashStorage() : address(0), Storage("Flash Storage") {}
 
 /**
  * @brief Locates the next available file block & tracks existing files
@@ -17,25 +17,39 @@ FlashStorage::FlashStorage() : position(0), Storage("Flash Storage") {}
  * reference. 
  */
 void FlashStorage::indexFlash() {
+  bool empty = this->isSectorEmpty();
   // Shuffle through data by 4KB until free space is reached
-  while (!isSectorEmpty() && (this->position < this->MAX_SIZE)) {
-    log_core("Sector " + String(this->position / this->SECTOR_SIZE) + " full");
-    this->position += this->SECTOR_SIZE; // 4 KB (shift to next block)
+  while (!empty && (this->address < this->MAX_SIZE)) {
+    // Check for file at sector start
+    if (!empty && this->readFileHeader()) {
+      this->file_data.push_back({++this->num_files, this->address});
+      log_core("File " + String(this->num_files) + " at address " + String(this->address));
+    }
+
+    // Log the occupied sector & iterate the address
+    log_core("Sector " + String(this->address / this->SECTOR_SIZE) + " occupied");
+    this->address += this->SECTOR_SIZE; // 4 KB (shift to next block)
+
+    // Check if the next sector is empty
+    empty = this->isSectorEmpty();
   }
+
+  // Write a file header at the beginning of the sector
+  this->writeFileHeader();
 }
 
 /**
- * @brief Locates the next available write position in flash.
+ * @brief Locates the next available write address in flash.
  *
- * Reads bytes from flash starting at the current position and increments
+ * Reads bytes from flash starting at the current address and increments
  * through memory until finding a free (0xFF) location or reaching the max size.
  */
-void FlashStorage::loadPosition() {
-  uint8_t currentByte = this->flash.readByte(this->position);
+void FlashStorage::loadAddress() {
+  uint8_t currentByte = this->flash.readByte(this->address);
 
-  // Shuffle through data at last recorded position until free space is reached
-  while ((currentByte != 0xFF) && (this->position < this->MAX_SIZE)) {
-    currentByte = this->flash.readByte(++this->position);
+  // Shuffle through data at last recorded address until free space is reached
+  while ((currentByte != 0xFF) && (this->address < this->MAX_SIZE)) {
+    currentByte = this->flash.readByte(++this->address);
   }
 }
 
@@ -50,7 +64,49 @@ void FlashStorage::loadPosition() {
 bool FlashStorage::isSectorEmpty() {
   // Check the next 16 bytes for 0xFF
   for (int i = 0; i < 16; ++i) {
-    if (this->flash.readByte(this->position + i) != 0xFF) return false;
+    if (this->flash.readByte(this->address + i) != 0xFF) return false;
+  }
+  return true;
+}
+
+/**
+ * @brief Writes the file header to the start of a new sector.
+ * 
+ * Write a file header (0xDEADBEEF) to indicate the start of a new file at the
+ * beginning of a sector. Then, store the file number, along with the start
+ * and end address in a data structure.
+ * 
+ */
+void FlashStorage::writeFileHeader() {
+  uint8_t num_bytes = sizeof(this->FILE_HEADER) * 8;
+
+  // Write 0xDEADBEEF to the start of the sector
+  while (num_bytes != 0) {
+    num_bytes -= 8;
+    this->flash.writeByte(this->address++, (this->FILE_HEADER >> num_bytes) & 0xFF);
+    this->flash.blockingBusyWait();
+  }
+
+  // Store necessary file data for quick reference
+  log_core("New file " + String(++this->num_files) + " at address " + String(this->address) + " created");
+  this->file_data.push_back({this->num_files, this->address - 4});
+}
+
+/**
+ * @brief Determines if the first 4 bytes of a sector are a file header.
+ * 
+ * @return true  - File header is present
+ * @return false - File header is not present
+ */
+bool FlashStorage::readFileHeader() {
+  uint8_t num_bytes = sizeof(this->FILE_HEADER) * 8;
+  
+  // Read 0xDEADBEEF from the start of the sector
+  for (int i = 0; i < sizeof(FILE_HEADER); ++i) {
+    num_bytes -= 8;
+    if (this->flash.readByte(this->address + i) != ((this->FILE_HEADER >> num_bytes) & 0xFF)) {
+      return false;
+    }
   }
   return true;
 }
@@ -59,7 +115,7 @@ bool FlashStorage::isSectorEmpty() {
  * @brief Verifies the flash connection and prepares for data storage.
  *
  * Attempts to initialize the flash memory. If successful, resets the internal
- * write position to 0, locates the next free position, and returns true.
+ * write address to 0, locates the next free address, and returns true.
  * Returns false otherwise.
  *
  * @return true  - Flash initialization is successful
@@ -85,22 +141,23 @@ bool FlashStorage::verify() {
   if (this->flash.begin(FLASH_CS_PIN) == false) return false;
 #endif
 
-  // Log initial position and find next available sector
-  log_core("Initial position: " + String(this->position));
-  this->position = this->DATA_START_POSITION;
-  this->indexFlash(); // Get position from flash
+  // Log initial address and find next available sector
+  log_core("Initial address: " + String(this->address));
+  this->address = this->START_ADDRESS;
+  this->num_files = 0;
+  this->indexFlash(); // Get address from flash and track files
 
   // Check if flash is full
-  if (this->position >= this->MAX_SIZE) {
+  if (this->address >= this->MAX_SIZE) {
     log_core("Flash memory is full.");
     return false;
   }
 
   // Log flash information
-  uint32_t sector = this->position / this->SECTOR_SIZE;
-  log_core("Updated position: " + String(this->position) + " in sector " + 
+  uint32_t sector = this->address / this->SECTOR_SIZE;
+  log_core("Updated address: " + String(this->address) + " in sector " + 
            String(sector));
-  log_core("Remaining space: " + String(this->MAX_SIZE - this->position) + " bytes");
+  log_core("Remaining space: " + String(this->MAX_SIZE - this->address) + " bytes");
 
   return true;
 }
@@ -109,7 +166,7 @@ bool FlashStorage::verify() {
  * @brief Stores a string in flash memory, appending a newline at the end.
  *
  * Iterates through each character of the string and writes it to flash. The
- * internal write position is then advanced accordingly.
+ * internal write address is then advanced accordingly.
  *
  * @param data - The string to be stored in flash.
  */
@@ -117,13 +174,12 @@ void FlashStorage::store(String data) {
   data = data + "\n";
 
   for (const uint8_t& character : data) {
-    this->flash.writeByte(this->position, character);
-    ++this->position;
+    this->flash.writeByte(this->address++, character);
     this->flash.blockingBusyWait();
   }
 
   log_core("Writing " + String(data.length()) + " bytes at " +
-           String(this->position));
+           String(this->address));
 
   this->flash.blockingBusyWait();
 }
@@ -145,14 +201,13 @@ void FlashStorage::storePacket(uint8_t* packet) {
 
   // Iterate through packet and write to flash
   for (uint16_t i = 0; i < packet_len; i++) {
-    this->flash.writeByte(this->position, packet[packet_header + i]);
-    ++this->position;
+    this->flash.writeByte(this->address++, packet[packet_header + i]);
     this->flash.blockingBusyWait();
   }
 
   // Log the number of bytes written
   log_core("Writing " + String(packet_len) + " bytes at " +
-           String(this->position));
+           String(this->address));
   this->flash.blockingBusyWait();
 }
 
@@ -165,15 +220,14 @@ void FlashStorage::storePacket(uint8_t* packet) {
  */
 void FlashStorage::dump() {
   log_core("\nStarting data transfer: ");
-  log_core("Position is at " + String(this->position));
+  log_core("Address is at " + String(this->address));
   char data = '^';
-  uint32_t pos = this->DATA_START_POSITION;
+  uint32_t pos = this->START_ADDRESS;
   // read until it hits end_code
-  while ((pos < this->MAX_SIZE) && (pos < this->position) && (data != 0xFF)) {
+  while ((pos < this->MAX_SIZE) && (pos < this->address) && (data != 0xFF)) {
     digitalWrite(HEARTBEAT_PIN_0, (pos & 0x60) != 0);
     digitalWrite(HEARTBEAT_PIN_1, (pos & 0x60) != 0);
-    data = this->flash.readByte(pos);
-    pos++;
+    data = this->flash.readByte(pos++);
     Serial.write(data);  // print as a character
     // delay(1);
   }
@@ -183,9 +237,9 @@ void FlashStorage::dump() {
  * @brief Erases all data stored in flash memory.
  *
  * Erases all data stored in flash memory by calling the flash erase function
- * and resetting the internal write position to 0.
+ * and resetting the internal write address to 0.
  */
 void FlashStorage::erase() {
   this->flash.erase();
-  this->position = 0;
+  this->address = 0;
 }
