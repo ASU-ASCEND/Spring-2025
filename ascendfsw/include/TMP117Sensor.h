36d7151ba5fea109577c65f48a84f6085e63319d
baykanda <k.bandsit@gmail.com>
2025-03-14 16:19:55 -0700

    implemented tmp117 sensor class

---
+++ ascendfsw/include/TMP117Sensor.h
@@ -0,0 +1,32 @@
+#ifndef TMP117_SENSOR_H
+#define TMP117_SENSOR_H
+
+#include <Wire.h>
+#include <SparkFun_TMP117.h>
+
+#include "Sensor.h"
+
+
+
+/**
+ * @brief Implementation of a Sensor for the TMP117
+ *
+ */
+class TMP117Sensor : public Sensor {
+ private:
+  TMP117 tmp;
+
+ public:
+  TMP117Sensor();
+  TMP117Sensor(unsigned long minium_period);
+  bool verify();
+  String readData();
+
+  // Function to integrate sensor data into a packet buffer.
+  void readDataPacket(uint8_t*& packet);
+
+  // Function to decode sensor data from a packet and return a CSV string
+  String decodeToCSV(uint8_t*& packet);
+};
+
+#endif
---
+++ ascendfsw/src/TMP117Sensor.cpp
@@ -0,0 +1,70 @@
+#include "TMP117Sensor.h"
+
+/**
+ * @brief Construct a new TMP117Sensor object with default minimum_period of 0 ms.
+ * 
+ */
+TMP117Sensor::TMP117Sensor() : TMP117Sensor(0){}
+
+/**
+ * @brief Construct a new TMP117Sensor object.
+ * 
+ * @param minimum_period Minimum time to wait between readings in ms
+ */
+TMP117Sensor::TMP117Sensor(unsigned long minimum_period) : Sensor ("TMP117", "TMP117Temp(C)", 1, minimum_period) {}
+
+/**
+ * @brief Returns if sensor is connected and working.
+ * 
+ * @return true if sensor is connected and working.
+ * @return false if sensor is not.
+ */
+bool TMP117Sensor::verify() {
+    Wire.begin();
+    return tmp.begin();
+}
+
+/**
+ * @brief Reads temperature in Celcius from TMP117.
+ * 
+ * @return String Temp (C) in csv format.
+ */
+String TMP117Sensor::readData() { 
+    float tempC = tmp.readTempC();
+    return String(tempC) + ","; 
+}
+
+/**
+ * @brief Reads sensor data (Temp (C)) and appends to the passed uint8_t array pointer, incrementing it.
+ * 
+ * @param packet Pointer to the packet byte array.
+ */
+void TMP117Sensor::readDataPacket(uint8_t*& packet) {
+    
+    float tempC = tmp.readTempC();
+
+    std::copy((uint8_t*)(&tempC), (uint8_t*)(&tempC) + sizeof(tempC), packet);
+
+    packet += sizeof(tempC);
+
+
+}
+
+/**
+ * @brief Decodes sensor data from the packet into a CSV string.
+ * 
+ * @param packet  Pointer to packet byte array that will be decoded.
+ * @return String Decoded sensor data in CSV format.
+ */
+String TMP117Sensor::decodeToCSV(uint8_t*& packet) {
+    
+    float tempC;
+    memcpy(&tempC, packet, sizeof(float));
+
+    packet += sizeof(float);
+
+    return String(tempC) + ",";
+}
+
+
+
