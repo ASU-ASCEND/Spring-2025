f94bd9cec02e41b142cdb37cf498d4c3b86d5f24
Matthew Arenivas <mrareniv@asu.edu>
2025-03-19 18:51:24 -0700

    ASCEND-87 Implement download functionality with cancel feature

--- docs/search/all_13.js
+++ docs/search/all_13.js
@@ -4,6 +4,11 @@ var searchData=
   ['tempsensor_1',['tempsensor',['../classTempSensor.html',1,'TempSensor'],['../classTempSensor.html#aa444370d1924832815f93188b79414cc',1,'TempSensor::TempSensor()'],['../classTempSensor.html#a64c43cf917c318fee62f2caa54e2647a',1,'TempSensor::TempSensor(unsigned long minium_period)']]],
   ['tempsensor_2ecpp_2',['TempSensor.cpp',['../TempSensor_8cpp.html',1,'']]],
   ['tempsensor_2eh_3',['TempSensor.h',['../TempSensor_8h.html',1,'']]],
-  ['toggle_4',['toggle',['../classErrorDisplay.html#a493bde338b369414abffe90e0d49e93b',1,'ErrorDisplay']]],
-  ['tools_5',['Data Processing Tools',['../index.html#autotoc_md7',1,'']]]
+  ['tmp_4',['tmp',['../classTMP117Sensor.html#a38882f972f4953676b9894f4ab6a15cd',1,'TMP117Sensor']]],
+  ['tmp117sensor_5',['tmp117sensor',['../classTMP117Sensor.html',1,'TMP117Sensor'],['../classTMP117Sensor.html#a6d094f004b8738177d667059fbb16ea0',1,'TMP117Sensor::TMP117Sensor()'],['../classTMP117Sensor.html#aa15ba70c71358fc857fa074e785242ec',1,'TMP117Sensor::TMP117Sensor(unsigned long minium_period)']]],
+  ['tmp117sensor_2ecpp_6',['TMP117Sensor.cpp',['../TMP117Sensor_8cpp.html',1,'']]],
+  ['tmp117sensor_2eh_7',['TMP117Sensor.h',['../TMP117Sensor_8h.html',1,'']]],
+  ['tmp_5fsensor_8',['tmp_sensor',['../main_8cpp.html#af1a189b3f408ea03e609977377d8a068',1,'main.cpp']]],
+  ['toggle_9',['toggle',['../classErrorDisplay.html#a493bde338b369414abffe90e0d49e93b',1,'ErrorDisplay']]],
+  ['tools_10',['Data Processing Tools',['../index.html#autotoc_md7',1,'']]]
 ];
